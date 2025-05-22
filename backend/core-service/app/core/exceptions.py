3d4f39ce498ddafe83c72d0e9d78861c6b93ec96
Алексей <alexeyisaev2@mail.ru>
2025-05-22 16:11:26 +0300

    small fix

---
+++ backend/core-service/app/core/exceptions.py
@@ -0,0 +1,37 @@
+from fastapi import HTTPException, status
+
+
+class RegistrationFailedException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Произошла ошибка при регистрации"
+        )
+
+class LoginAlreadyExistsException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Логин уже занят"
+        )
+
+class UnauthorizedException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Вы не авторизованы"
+        )
+
+class TokenMissingException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Токен отсутствует"
+        )
+
+class InvalidTokenException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Неверный или просроченный токен"
+        )
