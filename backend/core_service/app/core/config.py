2f61933150e122ed76622126bd8046990d52a6e7
Алексей <alexeyisaev2@mail.ru>
2025-05-25 20:59:39 +0300

    small fix

--- backend/core_service/app/core/config.py
+++ backend/core_service/app/core/config.py
@@ -8,8 +8,8 @@ from ..models.models import Accounts, Events, TicketTypes, Tickets
 # Загружаем переменные из .env
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+SECRET_KEY = os.getenv("SECRET_KEY", 'atgeg4wetg4ge')
+ALGORITHM = os.getenv("ALGORITHM", 'HS256')
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "10080"))
 
 GET_TABLE = {
