6e07ffaf8e362852337940abe148751cc79c71f0
Алексей <alexeyisaev2@mail.ru>
2025-05-26 23:20:29 +0300

    small fix

--- backend/core_service/app/core/config.py
+++ backend/core_service/app/core/config.py
@@ -8,12 +8,13 @@ from ..models.models import Accounts, Events, TicketTypes, Tickets
 # Загружаем переменные из .env
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", 'atgeg4wetg4ge')
-ALGORITHM = os.getenv("ALGORITHM", 'HS256')
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "10080"))
+SECRET_KEY: str = os.getenv("SECRET_KEY", 'atgeg4wetg4ge')
+ALGORITHM: str = os.getenv("ALGORITHM", 'HS256')
+ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "10080"))
 
-status_events: set = ('опубликовано', 'завершено', 'черновик')
+status_events: set = ('опубликовано', 'завершено', 'черновик') # FIXME: в user_tickets есть копия с использованием enum. потом сделаю масштабируемое
 
+# Для удобного взаимодействия алхимией
 GET_TABLE: dict = {
     'Accounts': Accounts,
     'Events': Events,
