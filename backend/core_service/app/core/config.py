f0764074a3eebd7c479ae65da2f1bd7f1a7c4ce5
Алексей <alexeyisaev2@mail.ru>
2025-06-01 20:33:05 +0300

    small fix

--- backend/core_service/app/core/config.py
+++ backend/core_service/app/core/config.py
@@ -11,19 +11,22 @@ load_dotenv()
 
 
 class Settings:
-    SECRET_KEY: str = os.getenv("SECRET_KEY", 'atgeg4wetg4ge')
-    ALGORITHM: str = os.getenv("ALGORITHM", 'HS256')
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 10080))
+    SECRET_KEY: str = os.getenv("SECRET_KEY", "atgeg4wetg4ge")
+    ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
+        os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 10080)
+    )
 
     # FIXME: в user_tickets есть копия с использованием enum. потом сделаю масштабируемое + crud.py
-    STATUS_EVENTS: frozenset[str] = frozenset({'опубликовано', 'завершено', 'черновик'})
+    STATUS_EVENTS: frozenset[str] = frozenset({"опубликовано", "завершено", "черновик"})
 
     # Для удобного взаимодействия c алхимией
     GET_TABLE: dict[str, Type[BaseModel]] = {
-        'Accounts': Accounts,
-        'Events': Events,
-        'TicketTypes': TicketTypes,
-        'Tickets': Tickets,
-    } # копия в crud.py
+        "Accounts": Accounts,
+        "Events": Events,
+        "TicketTypes": TicketTypes,
+        "Tickets": Tickets,
+    }  # копия в crud.py
+
 
 config = Settings()
