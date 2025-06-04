79b6c968ce659bf7f910bd5e99d040ca6b5ebcf3
Алексей <alexeyisaev2@mail.ru>
2025-06-04 12:52:12 +0300

    создал новую ручку

--- backend/core_service/app/api/v1/ticket_types.py
+++ backend/core_service/app/api/v1/ticket_types.py
@@ -59,3 +59,21 @@ async def edit_types_ticket(  # type: ignore[no-untyped-def]
     jwt_token: str = Cookie(None),
 ):
     return await service.edit_types_ticket(jwt_token, types_ticket_id, ticket_type_data)
+
+
+@router.delete(
+    "/{types_ticket_id}",
+    summary="",
+    description="",
+    status_code=status.HTTP_200_OK,
+    responses=None,  # TODO: дописать
+)
+@IClearCache(unique_name="ticket-types-cache", jwt_token_path="jwt_token")
+async def edit_types_ticket(  # type: ignore[no-untyped-def]
+    types_ticket_id: int = Path(
+        ..., title="ID типа билета мероприятия", ge=1, le=config.MAX_ID
+    ),
+    service: ManagementTicketTypeProtocol = Depends(get_ticket_types_service),
+    jwt_token: str = Cookie(None),
+):
+    return
